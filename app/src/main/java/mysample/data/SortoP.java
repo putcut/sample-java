e0e9bf0eb9f340e3007bb3dfea1c2ca5cee6accc
putcut <putcutpoint@gmail.com>
2021-04-19 12:36:01 +0000

    add sort sample

---
+++ app/src/main/java/mysample/data/SortoP.java
@@ -0,0 +1,14 @@
+package mysample;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SortoP {
+  private int id;
+  private String name;
+  private XMLGregorianCalendar datetime;
+}
